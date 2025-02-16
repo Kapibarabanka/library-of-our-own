0564ff912027e884cd6475240ede94b1a01761e1
kapibarabanka <kapibarabanka@users.noreply.github.com>
2025-02-16 15:47:09 +0100

    Revert "fix docker"
    This reverts commit 3bfd21eec409630e0c9f924f5372b449f7a2ec53.

--- lo3web/next.config.ts
+++ lo3web/next.config.ts
@@ -1,18 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
+  /* config options here */
 };
 
 export default nextConfig;
