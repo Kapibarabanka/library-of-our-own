4b0db066c6cec3f69f1aa4f0399a87edc6a8152f
kapibarabanka <kapibarabanka@users.noreply.github.com>
2025-02-16 15:47:09 +0100

    Revert "fix docker again"
    This reverts commit f0c31dd9e283807de6fdb255045d209e54be3302.

--- lo3web/src/app/library/page.tsx
+++ lo3web/src/app/library/page.tsx
@@ -3,6 +3,6 @@ import { getAllFics } from '@/utils/api-functions';
 import LibraryPage from './_components/LibraryPage/LibraryPage';
 
 export default async function Library() {
-    const page = await getAllFics(userId);
+    const page = await getAllFics(userId).catch(e => null);
     return <LibraryPage allCards={page?.cards ?? []}></LibraryPage>;
 }
