eb3a12fc27657cd349ece2f9c8f7e2452195570a
kapibarabanka <kapibarabanka@users.noreply.github.com>
2025-02-16 15:47:09 +0100

    Revert "add lo3web"
    This reverts commit 48a050199482ea4cdcf9070db37f0c1b8ff8a2b7.

--- lo3web/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Library from './library/page';
-
-export default function Home() {
-    return <Library />;
-}
--- lo3web/src/app/stats/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function Stats() {
-    return <div>Stats will be here</div>;
-}
