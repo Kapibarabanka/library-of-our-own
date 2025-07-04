bd2567bb1b18460162adc02f5f5ab2a68ee7c9c9
kapibarabanka <kapibarabanka@users.noreply.github.com>
2025-07-04 18:32:24 +0200

    use env for botname

--- lo3web/src/routes/login/+page.svelte
+++ lo3web/src/routes/login/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import { browser } from '$app/environment';
+    import { PUBLIC_BOTNAME } from '$env/static/public';
 </script>
 
 {#if browser}
     <script
         async
         src="https://telegram.org/js/telegram-widget.js?22"
-        data-telegram-login="TestKapibarabankaBot"
+        data-telegram-login={PUBLIC_BOTNAME}
         data-size="medium"
         data-auth-url="/login"
         data-request-access="write"
