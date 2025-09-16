7fd8be0779bd7651b31a1c5d8752a517843ffec8
kapibarabanka <kapibarabanka@users.noreply.github.com>
2025-09-16 14:05:17 +0200

    add ability to start fics from card

--- lo3web/src/app/library/FicCard.svelte
+++ lo3web/src/app/library/FicCard.svelte
@@ -9,27 +9,50 @@
     import { getTagsByField } from './_utils/filter-utils';
     import { pageState } from './state.svelte';
     import RefreshCw from 'lucide-svelte/icons/refresh-cw';
-    import Maximize from 'lucide-svelte/icons/maximize-2';
+    import Ellipsis from 'lucide-svelte/icons/ellipsis-vertical';
     import Tablet from 'lucide-svelte/icons/tablet';
     import Button from '$ui/button/button.svelte';
     import { goto } from '$app/navigation';
+    import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+    import { toast } from 'svelte-sonner';
+    import { startedToday } from '$api/fics-details.remote';
 
     let { cardData }: { cardData: FicCardData } = $props();
     const tagTypes = [TagField.Warning, TagField.Fandom, TagField.Ship, TagField.Character, TagField.Tag];
+
+    async function startReading() {
+        await startedToday(cardData.key);
+        toast('Marked as "In progress"');
+    }
 </script>
 
 <Card.Root>
-    <Card.Header class="pr-3">
+    <Card.Header>
         <Card.Title class="text-base">
             <div class="flex">
                 <a href={cardData.ao3Info.link} target="_blank" class="flex-1">{cardData.ao3Info.title}</a>
-                <Button
-                    variant="ghost"
-                    class="p-1 h-4"
-                    onclick={() => goto(`/fic/${cardData.key.ficType.toLowerCase()}-${cardData.key.ficId}`)}
-                >
-                    <Maximize class="text-muted-foreground" size={15}></Maximize>
-                </Button>
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                        {#snippet child({ props })}
+                            <Button variant="ghost" class="p-1 h-4" {...props}>
+                                <Ellipsis class="text-muted-foreground" size={15}></Ellipsis>
+                            </Button>
+                        {/snippet}
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content align="end">
+                        <DropdownMenu.Group>
+                            <DropdownMenu.Item
+                                onclick={() => goto(`/fic/${cardData.key.ficType.toLowerCase()}-${cardData.key.ficId}`)}
+                            >
+                                See details
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Item onclick={() => window.open(cardData.ao3Info.link, '_blank')}>
+                                Open on AO3
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Item onclick={() => startReading()}>Start reading</DropdownMenu.Item>
+                        </DropdownMenu.Group>
+                    </DropdownMenu.Content>
+                </DropdownMenu.Root>
             </div>
         </Card.Title>
         <Card.Description
--- lo3web/src/routes/(pages)/(authed)/+layout.svelte
+++ lo3web/src/routes/(pages)/(authed)/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import AppSidebar from '$lib/components/AppSidebar.svelte';
     import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+    import { Toaster } from '$ui/sonner';
     import '../../../app.css';
     let { children } = $props();
 </script>
+
+<Toaster />
 
 <Sidebar.Provider>
     <AppSidebar />
