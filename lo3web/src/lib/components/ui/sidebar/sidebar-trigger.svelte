f3784ab015f0d6171ffe99f275e68a8b3aec51cf
kapibarabanka <kapibarabanka@users.noreply.github.com>
2026-01-12 21:03:55 +0100

    version upgrade

--- lo3web/src/lib/components/ui/sidebar/sidebar-trigger.svelte
+++ lo3web/src/lib/components/ui/sidebar/sidebar-trigger.svelte
@@ -1,33 +1,35 @@
 <script lang="ts">
-    import { Button } from '$lib/components/ui/button/index.js';
-    import { cn } from '$lib/components/ui/utils.js';
-    import Menu from 'lucide-svelte/icons/menu';
-    import type { ComponentProps } from 'svelte';
-    import { useSidebar } from './context.svelte.js';
+	import { Button } from "$lib/components/ui/button/index.js";
+	import { cn } from "$lib/components/ui/utils.js";
+	import PanelLeftIcon from "@lucide/svelte/icons/panel-left";
+	import type { ComponentProps } from "svelte";
+	import { useSidebar } from "./context.svelte.js";
 
-    let {
-        ref = $bindable(null),
-        class: className,
-        onclick,
-        ...restProps
-    }: ComponentProps<typeof Button> & {
-        onclick?: (e: MouseEvent) => void;
-    } = $props();
+	let {
+		ref = $bindable(null),
+		class: className,
+		onclick,
+		...restProps
+	}: ComponentProps<typeof Button> & {
+		onclick?: (e: MouseEvent) => void;
+	} = $props();
 
-    const sidebar = useSidebar();
+	const sidebar = useSidebar();
 </script>
 
 <Button
-    type="button"
-    onclick={e => {
-        onclick?.(e);
-        sidebar.toggle();
-    }}
-    data-sidebar="trigger"
-    variant="ghost"
-    class={cn('h-10 w-10 [&_svg]:size-6', className)}
-    {...restProps}
+	data-sidebar="trigger"
+	data-slot="sidebar-trigger"
+	variant="ghost"
+	size="icon"
+	class={cn("size-7", className)}
+	type="button"
+	onclick={(e) => {
+		onclick?.(e);
+		sidebar.toggle();
+	}}
+	{...restProps}
 >
-    <Menu />
-    <span class="sr-only">Toggle Sidebar</span>
+	<PanelLeftIcon />
+	<span class="sr-only">Toggle Sidebar</span>
 </Button>
